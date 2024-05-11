f64d75d46706e2bbd31cd598af28d122638e7dea
eduard-biris <eduardandreibiris@yahoo.com>
2024-05-11 12:48:07 +0300

    Added multiple components import from Mf-app

--- src/App.tsx
+++ src/App.tsx
@@ -2,144 +2,17 @@ import React, { lazy, Suspense, useEffect, useState } from 'react';
 import useFetch from 'react-fetch-hook';
 
 const MicroFrontendsAppSupportedTypes = lazy(() => import('MicroFrontendsApp/SupportedTypes'));
-const Component = lazy(() => import('MicroFrontendsApp/Timelines/TimelineView'));
-
-/*
-  Ilustratii de implementat:
-  - Sunburst - ?
-  - Scatter - ?
-  - PieChart - DA
-  - LineChart - DA
-  - BarChart - DA
-  - Calendar - DA
-  
-  Ilustratii implementate:
-  - Timeline
-*/
+const Component = lazy(() => import('MicroFrontendsApp/Illustry/WraperComponent'));
 
 const SecondLibSupportedTypes = lazy(() => import('SecondMfApp/SupportedTypes'));
 const SecondLibComponent = lazy(() => import('SecondMfApp/Component')); 
 
-// 25 mai -> aplicatie + parte scrisa gata (draft)
+const { testData } = require('./test_data');
 
 function App() {
-
-  const data = {
-    "2023-10-07": {
-      "summary": {
-        "title": "Sample Timeline"
-      },
-      "events": [
-        {
-          "summary": "Event 1",
-          "date": "08:00:00",
-          "type": "Type A",
-          "author": "Author 1",
-          "tags": [
-            {
-              "name": "Tag A"
-            }
-          ],
-          "description": "Description of Event 1"
-        },
-        {
-          "summary": "Event 2",
-          "date": "09:00:00",
-          "type": "Type B",
-          "author": "Author 2"
-        }
-      ]
-    },
-    "2023-10-10": {
-      "summary": {
-        "title": "Sample Timeline"
-      },
-      "events": [
-        {
-          "summary": "Event 3",
-          "date": "09:00:00",
-          "type": "Type C",
-          "author": "Author 3"
-        },
-        {
-          "summary": "Event 4",
-          "date": "10:00:00",
-          "type": "Type D",
-          "author": "Author 4"
-        },
-        {
-          "summary": "Event 5",
-          "date": "10:00:00",
-          "type": "Type E",
-          "author": "Author 5"
-        }
-      ]
-    },
-    "2023-10-08": {
-      "summary": {
-        "title": "Sample Timeline"
-      },
-      "events": [
-        {
-          "summary": "Event 6",
-          "date": "11:00:00",
-          "type": "Type F",
-          "author": "Author 6"
-        },
-        {
-          "summary": "Event 7",
-          "date": "11:00:00",
-          "type": "Type G",
-          "author": "Author 7"
-        },
-        {
-          "summary": "Event 8",
-          "date": "12:00:00",
-          "type": "Type H",
-          "author": "Author 8"
-        }
-      ]
-    },
-    "2023-10-06 ": {
-      "summary": {
-        "title": "Sample Timeline"
-      },
-      "events": [
-        {
-          "summary": "Event 9",
-          "date": "12:00:00",
-          "type": "Type I",
-          "author": "Author 9"
-        },
-        {
-          "summary": "Event 10",
-          "date": "13:00:00",
-          "type": "Type J",
-          "author": "Author 10"
-        }
-      ]
-    }
-  };
-
   const { isLoading: visLoading, data: storedVis } = useFetch('http://localhost:8000/visualisations');
 
-  const [
-    componentStores,
-    setComponentStores
-  ] = useState([
-    // {
-    //   name: 'MicroFrontendsApp',
-    //   component: Component,
-    //   types: [],
-    // },
-    // {
-    //   name: 'SecondLibApp',
-    //   component: SecondLibComponent,
-    //   types: [],
-    // }
-  ]);
-
-
+  const [componentStores, setComponentStores] = useState([]);
   const [componentToRender, setComponentToRender] = useState();
 
   useEffect(() => {
@@ -152,10 +25,10 @@ function App() {
       storedVis.visualisations.forEach(vis => {
         console.log('vis: ', vis);
 
-        const type = vis.type;
-        // const type = 'TestComponentOne';
-        // const type = 'SecondLibTestTwo';
-        // const type = 'UnknownType';
+        // const type = vis.type;
+
+        const type = 'PiechartView';
+        const testProps = testData[type];
 
         const findComponentToRender = (type: string) => {
           for (const store of componentStores) {
@@ -171,7 +44,7 @@ function App() {
 
         console.log('Type: ', type);
 
-        setComponentToRender(<Component type={type} data={data} message={'my message'}/>)
+        setComponentToRender(<Component type={type} props={testProps}/>)
       });
     }
   }, [componentStores, storedVis]);
