8d459b1911b1a951c4ad16fbf53670bc6e78b600
eduard-biris <eduardandreibiris@yahoo.com>
2024-05-29 11:51:10 +0300

    Fixed type warnings. Added basic styling and layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,28 @@ import useFetch from 'react-fetch-hook';
 const MicroFrontendsAppSupportedTypes = lazy(() => import('MicroFrontendsApp/SupportedTypes'));
 const WraperComponent = lazy(() => import('MicroFrontendsApp/Illustry/WraperComponent'));
 
+import './App.css';
+
 function App() {
-  const { isLoading: visLoading, data: storedVis } = useFetch('http://localhost:8000/visualisations');
+  const { data: storedVis } = useFetch('http://localhost:8000/visualisations');
 
   const [mfSupportedTypes, setMfSupportedTypes] = useState();
   const [supportedVisualisations, setSupportedVisualisations] = useState();
 
+  const [currentVisualization, setCurrentVisualization] = useState();
+
   useEffect(() => {
-    if(storedVis?.visualisations?.length && mfSupportedTypes?.length) {
+    if((storedVis as any)?.visualisations?.length && (mfSupportedTypes as any)?.length) {
       console.log('In if: ', mfSupportedTypes, storedVis);
       const supportedVisualisationsArray = [];
 
-      storedVis.visualisations.forEach((vis) => {
-        if(mfSupportedTypes.includes(vis.visualization.type)) {
+      (storedVis as any).visualisations.forEach((vis) => {
+        if((mfSupportedTypes as any).includes(vis.visualization.type)) {
           supportedVisualisationsArray.push(vis);
         }
       })
 
-      setSupportedVisualisations(supportedVisualisationsArray);
+      setSupportedVisualisations((supportedVisualisationsArray as any));
     }
   }, [mfSupportedTypes, storedVis]);
 
@@ -36,15 +40,43 @@ function App() {
         }}
       />
 
-        { supportedVisualisations?.length && supportedVisualisations.map((vis) => {
-            return (
-              <div>
-                {vis._id} - {vis.visualization.type}
-                <WraperComponent type={vis.visualization.type} props={vis.visualization.data} />
-              </div>
-            );
-          }) 
+      <div style={{width: '90vw', marginLeft: 'auto', marginRight: 'auto'}}>
+        <h1 className='app-header'>
+          {currentVisualization ? (currentVisualization as any).visualization.name : 'Available Visualizations' }
+        </h1>
+
+        {
+          currentVisualization && <button 
+            className='button'
+            onClick={() => {
+              setCurrentVisualization(null);
+            }}
+          >
+            Back
+          </button>
         }
+
+        {!currentVisualization && <ul>
+          { (supportedVisualisations as any)?.length && (supportedVisualisations as any).map((vis) => {
+              return (
+                <li className='list-row'>
+                  <h3 className='vis-header'>{vis.visualization.type}</h3>
+                  <h3 className='vis-header vis-name'>{vis.visualization.name}</h3>
+                  <button className='button'
+                    onClick={() => {setCurrentVisualization(vis)}}
+                  >
+                    View Visualization
+                  </button>
+                </li>
+              );
+            }) 
+          }
+        </ul>}
+      </div>
+
+      { currentVisualization &&
+        <WraperComponent type={(currentVisualization as any).visualization.type} props={(currentVisualization as any).visualization.data} />
+      } 
 
     </Suspense>
   );
